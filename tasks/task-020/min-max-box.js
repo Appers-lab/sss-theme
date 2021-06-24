5132c7a140049443c7674d96f1d8962ecb438e56
pmvald <pmvald@yahoo.com>
2021-06-24 09:58:46 -0700

    enhanced

--- tasks/task-020/min-max-box.js
+++ tasks/task-020/min-max-box.js
@@ -1,4 +1,6 @@
+
 class MinMaxBox extends HTMLElement {
+
   static get observedAttributes() {
     return ["min-value", "max-value"];
   }
@@ -50,16 +52,13 @@ class MinMaxBox extends HTMLElement {
   get value() {
     // TODO: handle error cases better
 
-    let min = this.minInput.value;
-    let max = this.maxInput.value;
+    let min = this.minInput.value || undefined;
+    let max = this.maxInput.value || undefined;
 
     if (!min && !max) return {};
     else if (min && max && min > max) return {};
     else
-      return {
-        min: min ? min : undefined,
-        max: max ? max : undefined,
-      };
+      return {min, max};
   }
 
   set value(val) {
@@ -79,49 +78,96 @@ class MinMaxBox extends HTMLElement {
   }
 
   render() {
+
     let minExp = this.noNegative ? `min="0"` : ``;
+
+    let specificStyles;
+
+    if (getComputedStyle(this).getPropertyValue("--layout").trim()==="row") {
+      specificStyles = `
+      
+      #main-wr {
+        display: flex; 
+        align-items: center;
+      }
+      
+      #first-input-wr,
+      #second-input-wr {
+        margin-right: var(--input-sections-gutter-width, 1rem);
+      }
+      
+      label {
+        margin-right: var(--input-label-gutter-width, 0.5rem);
+      }
+      
+      `;
+    }
+    else { //if (this.style.getPropertyValue("--layout")==="column")
+      specificStyles = `
+      #first-input-wr,
+      #second-input-wr {
+        margin-bottom: var(--input-sections-gutter-width, 1rem);
+        display: flex;
+        align-items: center;
+      }
+      
+      input, label {
+        display: block;
+      }
+      
+      label {
+        width: 3rem;
+      }      
+      `;
+    }
 
     this.shadowRoot.innerHTML = `
       <style>
         label {
-          margin-right: 0.5rem;
+            font-size: var(--text-font-size, inherit);       
+            color: var(--text-color, inherit);
         }
-        .first-input-wr,
-        .second-input-wr {
-          margin-right: 1rem;
-        }
+        
         input {
-          max-width: 120px;
+          max-width: var(--input-max-width, 120px);
           padding: 0.6rem;
           border-radius: 0.25rem;
-          font-size: var(--input-font-size);
+          font-size: var(--input-font-size, inherit);
           border: 1px solid #aaa;
         }
         button {
-          color: var(--button-color);
-          border: none;
-          background-color: inherit;
-          cursor: pointer;
-          font-size: 16px;
+          color: var(--clear-btn-color, inherit);
+          background-color: transparent;
+          padding: unset;
+          font-size: var(--text-font-size, inherit);
+          border: none;          
+          cursor: pointer;          
         }
         button:hover {
-          color: var(--button-color-hover);
+          color: var(--clear-btn-color-hover, inherit);
         }
+        
+        ${specificStyles}
+        
       </style>
 
-      <div style="display: flex; align-items: center">
-        <label>${this.minLabel}</label>
-        <div class="first-input-wr">
-          <input type="number" placeholder="${this.minPlaceholder}" role="min" ${minExp}>
+      <article id="main-wr">
+        
+        <div id="first-input-wr">
+            <label>${this.minLabel}</label>
+            <input type="number" placeholder="${this.minPlaceholder}" role="min" ${minExp}>
+        </div>        
+        
+        <div id="second-input-wr">
+            <label>${this.maxLabel}</label>
+            <input type="number" placeholder="${this.maxPlaceholder}" role="max" ${minExp}>                
         </div>
-        <label>${this.maxLabel}</label>
-        <div class="second-input-wr">
-          <input type="number" placeholder="${this.maxPlaceholder}" role="max" ${minExp}>                
-        </div>
+        
         <div>
           <button> Clear </button>            
-        </div>                        
-      </div>
+        </div>
+                                
+      </article>
     `;
   }
 };
